2b13d90b62a89ed63732d2ba4619ac8c2446a006
Ishan Karmakar <ishan.karmakar24@gmail.com>
2022-05-03 09:53:09 +0000

    Add DB tests

--- db/db_test.go
+++ db/db_test.go
@@ -2,11 +2,31 @@ package db
 
 import (
 	"os"
+	"path"
+	"runtime"
 	"testing"
 
+	"github.com/monkeswag33/noter-go/global"
 	"github.com/monkeswag33/noter-go/types"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+func changeDir(location string) string {
+	_, current, _, _ := runtime.Caller(0)
+	down := path.Join(path.Dir(current), location)
+	if err := os.Chdir(down); err != nil {
+		logrus.Fatal(err)
+	}
+	current, _ = os.Getwd()
+	return current
+}
+
+func TestMain(m *testing.M) {
+	changeDir("..")
+	code := m.Run()
+	os.Exit(code)
+}
 
 func TestGetLogLevel(t *testing.T) {
 	var database DB
@@ -23,23 +43,45 @@ func TestInitTesterDB(t *testing.T) {
 }
 
 func TestInit(t *testing.T) {
-	_, exists := os.LookupEnv("POSTGRES_URI")
-	t.Fatal(exists)
-	// global.SetupViper()
-	// var database DB = DB{
-	// 	LogLevel: types.LogLevelParams{
-	// 		LogLevel:     "warn",
-	// 		GormLogLevel: "warn",
-	// 	},
-	// }
-	// assert.NoError(t, database.Init())
+	global.SetupViper()
+	var database DB = DB{
+		LogLevel: types.LogLevelParams{
+			GormLogLevel: "warn",
+		},
+	}
+	assert.NoError(t, database.Init())
+	_, err := database.GetUsers(User{})
+	assert.NoError(t, err)
 }
 
-// func TestSetupDB(t *testing.T) {
-// 	global.SetupViper()
-// 	assert.NoError(t, SetupDB(types.LogLevelParams{
-// 		LogLevel:     "warn",
-// 		GormLogLevel: "warn",
-// 	}))
-// 	assert.IsType(t, &DB{}, Database)
-// }
+func TestSetupDB(t *testing.T) {
+	global.SetupViper()
+	assert.NoError(t, SetupDB(types.LogLevelParams{
+		GormLogLevel: "warn",
+	}))
+	assert.IsType(t, &DB{}, Database)
+	_, err := Database.GetUsers(User{})
+	assert.NoError(t, err)
+}
+
+func TestClose(t *testing.T) {
+	SetupDB(types.LogLevelParams{
+		GormLogLevel: "warn",
+	})
+	assert.NoError(t, Database.Close())
+	sqlDB, err := Database.DB.DB()
+	assert.NoError(t, err)
+	assert.Error(t, sqlDB.Ping()) // Ping should error out if connection was closed
+}
+
+func TestShutdownDB(t *testing.T) {
+	SetupDB(
+		types.LogLevelParams{
+			GormLogLevel: "warn",
+		},
+	)
+	assert.NoError(t, ShutdownDB())
+	if Database != nil {
+		t.Fatal("Database is not null")
+	}
+}
